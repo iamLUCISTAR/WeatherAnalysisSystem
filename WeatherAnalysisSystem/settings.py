8bc59951fc2b19e08cfaf92ca7473b886500f68a
Sharath B <bsharath2000@gmail.com>
2024-09-18 10:43:51 +0530

    Deployment changes

--- WeatherAnalysisSystem/settings.py
+++ WeatherAnalysisSystem/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ffowg-877+=_uxv1p$8qmjbixlnq%$s-_ex#s=d7u#h13s=z@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://weatheranalysissystem-u7j9.onrender.com']
 
